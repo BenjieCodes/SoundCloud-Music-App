51828b6e27735f73ad65fff29c1fe59d445ecaa5
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-23 16:56:16 -0400

    Added a working audio tag for the artists results element.

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ var token = '41f6b5a26693fd92184ddd76aaeef8ef'
 // this variable gives you the number of content you'd like (I put 5)
 var numOfObjects = '&limit=20'
 // this is the base url + the variables to grab the content/api endpoints
+var searchBarInput = '';
+
 var url = 'https://api.soundcloud.com/tracks?client_id=' + token + numOfObjects + '&q=John Mayer'
 
 
@@ -35,6 +37,6 @@ function getArtists (artists) {
   <img src="${artists.artwork_url}">
   <p> ${artists.title}</p>
   <p> ${artists.user.username}</p>
-  <audio src="${artists.stream_url}?client_id=${token}"></audio>
+  <audio controls src="${artists.stream_url}?client_id=${token}"></audio>
   `;
 }

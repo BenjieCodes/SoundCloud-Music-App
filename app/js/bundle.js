f047f4a466b13ce8123feb6dea3fc3833184a807
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-02 20:58:15 -0400

    Redoing CSS for project

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -9852,67 +9852,6 @@ var _jquery = require('jquery');
 var _jquery2 = _interopRequireDefault(_jquery);
 
 console.log(_jquery2['default']);
-//
-// import _ from 'lodash';
-// console.log(_.VERSION);
-//
-// // This token allows you to use api contents
-// var token = '41f6b5a26693fd92184ddd76aaeef8ef';
-// // this variable gives you the number of content you'd like (I put 5)
-// var numOfObjects = '&limit=15';
-// // 3. button for search
-// $('input').on('submit', function(event) {
-//   event.preventDefault();
-//
-// // this variable is used with the 'q' filter search parameter
-// var searchBarInput = $('input').val();
-// // this is the base url + the variables to grab the content/api endpoints
-// var url = 'https://api.soundcloud.com/tracks?client_id=' + token + numOfObjects + '&q=' + searchBarInput;
-//
-// // 1. This is used to pull data out from the web API using getSON and .then
-// $.getJSON(url).then(function (response) {
-//   console.log(response);
-//   response.forEach (function (res){
-//     var html = getArtists(res);
-//     $('.resultscontainer').append(html);
-//     var html = getSongs(res)
-//     // $('.audiocontainer').append(html);
-//     console.log(html);
-//   })
-// });
-//
-// });
-//
-// // 2. function created to pull results for the album artwork, title of song, and username
-// function getArtists (artists) {
-// $('.resultscontainer').empty();
-//   if (artists.artwork_url === null) {
-//   `<img src="http://placehold.it/100x100">
-//   `;
-//   }
-//   else {
-//   `
-//   <div class="artistsection">
-//   <img src="${artists.artwork_url}">
-//   <p> ${artists.title}</p>
-//   <p> ${artists.user.username}</p>
-//   </div>
-//   `;
-// }
-// }
-//
-// // 4. function created to pull audio file from artists clicked
-// function getSongs (songs) {
-//   return  `<audio controls src=${songs.stream_url}?client_id=${token}></audio>`;
-//
-// }
-//
-//
-// function songs (stream) {
-//
-//
-//
-// }
 
 var audio = (0, _jquery2['default'])('audio');
 var searchForm = (0, _jquery2['default'])('form');
@@ -9951,7 +9890,7 @@ function trackTemplate(track) {
   if (track.artwork_url === null) {
     track.artwork_url = 'http://placehold.it/100x100';
   }
-  return '\n        <div class="artistsection">\n          <span>' + track.stream_url + '</span>\n          <img src=' + track.artwork_url + '>\n          <p> ' + track.title + '</p>\n        </div>\n        ';
+  return '\n        <div class="artistsection">\n          <span>' + track.stream_url + '</span>\n          <img src=' + track.artwork_url + '>\n          <p> Title: ' + track.title + '</p>\n        </div>\n        ';
 }
 
 },{"jquery":1}]},{},[2])
--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,5 @@
 import $ from 'jquery';
 console.log($);
-//
-// import _ from 'lodash';
-// console.log(_.VERSION);
-//
-// // This token allows you to use api contents
-// var token = '41f6b5a26693fd92184ddd76aaeef8ef';
-// // this variable gives you the number of content you'd like (I put 5)
-// var numOfObjects = '&limit=15';
-// // 3. button for search
-// $('input').on('submit', function(event) {
-//   event.preventDefault();
-//
-// // this variable is used with the 'q' filter search parameter
-// var searchBarInput = $('input').val();
-// // this is the base url + the variables to grab the content/api endpoints
-// var url = 'https://api.soundcloud.com/tracks?client_id=' + token + numOfObjects + '&q=' + searchBarInput;
-//
-// // 1. This is used to pull data out from the web API using getSON and .then
-// $.getJSON(url).then(function (response) {
-//   console.log(response);
-//   response.forEach (function (res){
-//     var html = getArtists(res);
-//     $('.resultscontainer').append(html);
-//     var html = getSongs(res)
-//     // $('.audiocontainer').append(html);
-//     console.log(html);
-//   })
-// });
-//
-// });
-//
-// // 2. function created to pull results for the album artwork, title of song, and username
-// function getArtists (artists) {
-// $('.resultscontainer').empty();
-//   if (artists.artwork_url === null) {
-//   `<img src="http://placehold.it/100x100">
-//   `;
-//   }
-//   else {
-//   `
-//   <div class="artistsection">
-//   <img src="${artists.artwork_url}">
-//   <p> ${artists.title}</p>
-//   <p> ${artists.user.username}</p>
-//   </div>
-//   `;
-// }
-// }
-//
-// // 4. function created to pull audio file from artists clicked
-// function getSongs (songs) {
-//   return  `<audio controls src=${songs.stream_url}?client_id=${token}></audio>`;
-//
-// }
-//
-//
-// function songs (stream) {
-//
-//
-//
-// }
 
 var audio = $('audio');
 var searchForm = $('form');
@@ -107,7 +46,7 @@ function trackTemplate (track) {
         <div class="artistsection">
           <span>${track.stream_url}</span>
           <img src=${track.artwork_url}>
-          <p> ${track.title}</p>
+          <p> Title: ${track.title}</p>
         </div>
         `;
 }
